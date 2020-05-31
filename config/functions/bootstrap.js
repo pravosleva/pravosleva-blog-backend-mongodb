b8281e74635a9690c337719ee77cc75480030212
Den Pol <selection4test@gmail.com>
2020-06-01 00:29:06 +0300

    master. io experience

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -10,17 +10,33 @@
  * See more details here: https://strapi.io/documentation/3.0.0-beta.x/configurations/configurations.html#bootstrap
  */
 
-module.exports = cb => {
-  // import socket io
-  var io = require('socket.io')(strapi.server);
+// const geoip = require('geoip-lite')
+const users = new Map()
 
-  // listen for user connection
+module.exports = cb => {
+  var io = require('socket.io')(strapi.server)
+
+  // Listen for user connection
   io.on('connection', function(socket){
-    // send message on user connection
-    socket.emit('HELLO', { message: 'hello socket' });
-    // listen for user diconnect
-    socket.on('disconnect', () => console.log('somebody disconnected'));
-  });
-  strapi.io = io; // register socket io inside strapi main object to use it globally anywhere
-  if (cb) cb();
+    const clientIp = socket.request.connection.remoteAddress
+    const clientId = socket.id
+
+    users.set(clientId, clientIp)
+    socket.broadcast.emit('SOMEBODY_CONNECTED', { msg: `${clientId} connected. Total connected users: ${users.size}` })
+
+    // Send message on user connection
+    socket.emit('HELLO', { msg: `hello socket, your ID is ${clientId}, your IP is ${clientIp}` })
+
+    socket.on('reconnect', (attemptNumber) => {
+      socket.broadcast.emit('SOMEBODY_RECONNECTED', { msg: `${clientId} reconnected ${attemptNumber}, has users= ${users.has(clientId)}. Total connected users: ${users.size}` })
+    });
+    // Listen for user diconnect
+    socket.on('disconnect', (socket) => {
+      console.log(socket)
+      users.delete(clientId)
+      io.emit('SOMEBODY_DISCONNECTED', { msg: `${clientId} disconnected. Total connected users: ${users.size}` })
+    })
+  })
+  strapi.io = io // register socket io inside strapi main object to use it globally anywhere
+  if (cb) cb()
 };
